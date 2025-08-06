0cb3f995a53e8cc5ee31ae5e2149e3551bf52581
weiwilliam <weiwilliam1987@gmail.com>
2025-08-06 02:15:36 +0000

    solve conflicts and updates ignore list

--- Notebooks/step1.py
+++ Notebooks/step1.py
@@ -1,43 +1,26 @@
+import numpy as np
 from owslib.wms import WebMapService
 import lxml.etree as xmltree
 import xml.etree.ElementTree as xmlet
 import requests
 from skimage import io
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mticker
-import cartopy.crs as ccrs
-import cartopy
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
-def get_OCI_PACE_truecolor(time, bbox):
+def get_OCI_PACE_truecolor(time, size=(400, 800), bbox=(-180, -90, 180, 90)):
     """
-      time in format of YYYY-MM-DD
+      time: in format of YYYY-MM-DD
+      size: (height, width)
+      bbox: bounding box (minlon, minlat, maxlon, maxlat)
     """
+    height, width = size
     minlon, minlat, maxlon, maxlat = bbox
     #  Construct Geographic projection URL.
-    proj4326 = f'https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?version=1.3.0&service=WMS&request=GetMap&format=image/png&STYLE=default&bbox={int(minlat)},{int(minlon)},{int(maxlat)},{int(maxlon)}&CRS=EPSG:4326&HEIGHT=600&WIDTH=600&TIME={time}&layers=OCI_PACE_True_Color'
+    proj4326 = f'https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?version=1.3.0&service=WMS&request=GetMap&format=image/png&STYLE=default&bbox={int(minlat)},{int(minlon)},{int(maxlat)},{int(maxlon)}&CRS=EPSG:4326&HEIGHT={height}&WIDTH={width}&TIME={time}&layers=OCI_PACE_True_Color'
     
     # Request image.
     img = io.imread(proj4326)
+    x = np.linspace(minlon, maxlon, img.shape[0])
+    y = np.linspace(minlat, maxlat, img.shape[1])
+    print(img.shape)
+    # img = img[::-1, :]
 
-    # Display image on map.
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-    extent = (minlon, maxlon, minlat, maxlat)
-    plt.imshow(img, transform = ccrs.PlateCarree(), extent = extent, origin = 'upper')
-    
-    # Draw grid.
-    gl = ax.gridlines(ccrs.PlateCarree(), linewidth = 1, color = 'blue', alpha = 0.3,  draw_labels = True)
-    gl.top_labels = False
-    gl.right_labels = False
-    gl.xlines = True
-    gl.ylines = True
-    # gl.xlocator = mticker.FixedLocator([0, 30, -30, 0])
-    # gl.ylocator = mticker.FixedLocator([-30, 0, 30])
-    gl.xformatter = LONGITUDE_FORMATTER
-    gl.yformatter = LATITUDE_FORMATTER
-    gl.xlabel_style = {'color': 'blue'}
-    gl.ylabel_style = {'color': 'blue'}
-    
-    plt.show()
-    return img
+    return x, y, img
